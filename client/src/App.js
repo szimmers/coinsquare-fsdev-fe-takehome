308870cc7a7fa592b9c7e43c925b1c7dc02b3806
Steve Zimmers <steve@soccer-1.com>
2018-04-01 09:38:20 -0400

    - set up simple test server - add axios to client, test test server

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,32 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	componentWillMount() {
+		axios.get('/api')
+			.then(function (response) {
+				console.log('r:', response.data);
+			})
+			.catch(function (error) {
+				console.log('e:', error);
+			});
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo"/>
+					<h1 className="App-title">Welcome to React</h1>
+				</header>
+				<p className="App-intro">
+					To get started, edit <code>src/App.js</code> and save to reload.
+				</p>
+			</div>
+		);
+	}
 }
 
 export default App;
